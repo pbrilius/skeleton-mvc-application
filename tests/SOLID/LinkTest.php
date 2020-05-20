1b8f960f9e27e0b177c7e62df23b008c5cd0ae12
Povilas Brilius <pbrilius@gmail.com>
2020-05-20 16:31:12 +0300

    Link unit.

---
+++ tests/SOLID/LinkTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace Tests;
+
+use PBG\Entity\Link;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Link unit
+ * 
+ * @category Unit_Cases
+ * @package  SOLID
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class LinkTest extends TestCase
+{
+    /**
+     * Link case
+     *
+     * @return void
+     */
+    public function testId()
+    {
+        $link = new Link();
+
+        $this->assertObjectHasAttribute('_id', $link);
+    }
+
+    /**
+     * Label case
+     *
+     * @return void
+     */
+    public function testLabel()
+    {
+        $link = new Link();
+
+        $this->assertObjectHasAttribute('_label', $link);
+    }
+
+    /**
+     * Reference case
+     *
+     * @return void
+     */
+    public function testReference()
+    {
+        $link = new Link();
+
+        $this->assertObjectHasAttribute('_reference', $link);
+    }
+
+}
