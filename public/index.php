0f568b078218574e7e7d05075c1de35afcee1641
Povilas Brilius <pbrilius@gmail.com>
2020-05-20 18:45:11 +0300

    Revert "API added to index - main launch."
    This reverts commit f334d65e07bd7db1b137b6f2bb82a23511b94484.

--- public/index.php
+++ public/index.php
@@ -14,7 +14,6 @@
 
 require_once __DIR__ . '/../bootstrap/container.php';
 require_once __DIR__ . '/../bootstrap/routes.php';
-require_once __DIR__ . '/../bootstrap/api.php';
 
 $response = $router->dispatch($request);
 
