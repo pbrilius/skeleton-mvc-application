2d7d63d06237cfde833d5aed192c650b7220bb16
Povilas Brilius <pbrilius@gmail.com>
2020-05-20 23:00:43 +0300

    Non working Doctrine unveiled and exposed. It's a malware, based on this data, which is a strange cause, worth separate distancing.

---
+++ fusionote PBG Session.sql
@@ -0,0 +1,7 @@
+INSERT INTO `fusionote_pbg_website`.`api` (`id`, `release`, `version`)
+VALUES
+  (
+    'ce4fb406-a644-4a71-b150-578aaeda1375',
+    '2020-05-20T21:49:51+03:00',
+    '45c48cce'
+  );
