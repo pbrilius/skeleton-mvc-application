f334d65e07bd7db1b137b6f2bb82a23511b94484
Povilas Brilius <pbrilius@gmail.com>
2020-05-20 18:40:39 +0300

    API added to index - main launch.

--- public/index.php
+++ public/index.php
@@ -14,6 +14,7 @@
 
 require_once __DIR__ . '/../bootstrap/container.php';
 require_once __DIR__ . '/../bootstrap/routes.php';
+require_once __DIR__ . '/../bootstrap/api.php';
 
 $response = $router->dispatch($request);
 
