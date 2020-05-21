6c75eee0bce517586a10d35435e2df3e2c7bf2f2
Povilas Brilius <pbrilius@gmail.com>
2020-05-22 01:33:27 +0300

    API controller POST HTTP unit.

---
+++ tests/Controller/API/API/APIPostTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace Tests\API\API;
+
+use PBG\Controller\API\API\ApiPost;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Controller API - API singular test case
+ * 
+ * @category Unit_Cases
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class APIPostTest extends TestCase
+{
+    /**
+     * Correct return type test
+     *
+     * @return void
+     */
+    public function testArrayReturn(): void
+    {
+        $pdo = $this->prophesize(\PDO::class);
+        $api = new ApiPost($pdo->reveal());
+
+        $serverRequest = $this->prophesize(ServerRequestInterface::class);
+        $result = call_user_func($api, $serverRequest->reveal());
+
+        $this->assertIsArray($result);
+    }
+}
