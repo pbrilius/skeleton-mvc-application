fe95567d15eb548d5c56ce13e1e5a90f7fe53981
Povilas Brilius <pbrilius@gmail.com>
2020-05-23 01:07:07 +0300

    Note delete API atomics.

---
+++ src/PBGroup/Controller/API/Note/NoteDelete.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Controller
+ * @package  Invokables
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+
+namespace PBG\Controller\API\Note;
+
+use PBG\Controller\BaseController;
+
+/**
+ * Note stack API
+ * 
+ * @category API
+ * @package  Note
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class NoteDelete extends BaseController
+{
+    /**
+     * Inherited
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request Request
+     * @param array                                    $args    Arguments
+     * 
+     * @inheritDoc
+     * 
+     * @return array
+     */
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, array $args): array
+    {
+        /**
+         * PDO
+         * 
+         * @var \PDO $pdo PDO
+         */
+        $pdo = $this->getPdo();
+        $stmt = $pdo->prepare('DELETE FROM `note` WHERE `id` = :id');
+        $stmt->execute(
+            [
+                ':id' => $args['id'],
+            ]
+        );
+
+        return [];
+    }
+}
