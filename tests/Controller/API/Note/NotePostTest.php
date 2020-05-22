d6eb95937497382099890b9811ad0566e3e50ef3
Povilas Brilius <pbrilius@gmail.com>
2020-05-23 00:54:17 +0300

    Note post case and controller - dichotomy; API.

---
+++ tests/Controller/API/Note/NotePostTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Controller
+ * @package  Invokables
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace Tests\API\Note;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Note stack API
+ * 
+ * @category API
+ * @package  Note
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class NotePostTest extends TestCase
+{
+    /**
+     * Array return case
+     *
+     * @return void
+     */
+    public function testArrayReturn(): void
+    {
+        
+    }
+}
