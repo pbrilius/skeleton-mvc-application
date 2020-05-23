e956e76faebdd26b3c5d175ba83e5333333d7ed3
Povilas Brilius <pbrilius@gmail.com>
2020-05-23 15:26:59 +0300

    Image singular - API atomics.

---
+++ tests/Controller/API/Image/ImageTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+
+namespace Tests\API\Image;
+
+use PBG\Controller\API\Image\Image;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Image stack
+ * 
+ * @category Unit_Cases
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class ImageTest extends TestCase
+{
+    /**
+     * A{O endpoint}
+     *
+     * @return void
+     */
+    public function testArrayReturn(): void
+    {
+        $image = $this
+            ->getMockBuilder(Image::class)
+            ->disableOriginalConstructor()
+            ->disableProxyingToOriginalMethods()
+            ->setMethods(['__invoke'])
+            ->getMock();
+
+        $request = $this->prophesize(ServerRequestInterface::class);
+        $response = call_user_func($image, $request->reveal(), []);
+
+        $this->assertIsArray($response);
+    }
+}
