ee771338cb3d214158f1c6a14f272939365a8d59
Povilas Brilius <pbrilius@gmail.com>
2020-05-23 19:43:20 +0300

    Link Delete atomics business logistics case.

---
+++ src/PBGroup/Controller/API/Link/LinkDelete.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Controller
+ * @package  Invokables
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+
+namespace PBG\Controller\API\Link;
+
+use PBG\Controller\BaseController;
+
+/**
+ * Link stack
+ * 
+ * @category API
+ * @package  Link
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class LinkDelete extends BaseController
+{
+    /**
+     * Invocation
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request Request
+     * @param array                                    $args    Arguments
+     * 
+     * @inheritDoc
+     * 
+     * @return array
+     */
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, array $args): array
+    {
+        /**
+         * PDO
+         * 
+         * @var \PDO $pdo PDO
+         */
+        $pdo = $this->getPdo();
+        $stmt = $pdo->prepare(
+            'DELETE FROM `link` '
+                . 'WHERE `id` = :id'
+        );
+        $stmt->execute(
+            [
+                ':id' => $args['id'],
+            ]
+        );
+
+        return [];
+    }
+}
