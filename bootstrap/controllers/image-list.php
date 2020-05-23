abd759d36bb498c7e00e207dd187d65f1096275e
Povilas Brilius <pbrilius@gmail.com>
2020-05-23 17:21:43 +0300

    Image containerized - API.

---
+++ bootstrap/controllers/image-list.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Load
+ * @package  Launch
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+
+use League\Container\Container;
+use PBG\Controller\API\Image\ImageList;
+
+/**
+ * Container
+ * 
+ * @var Container $container
+ */
+$container->add(
+    ImageList::class,
+    function () use ($container) {
+        $imageList = new ImageList($container->get('mysql.pdo')[0]);
+
+        return $imageList;
+    }
+);
