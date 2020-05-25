e524dc3b4f00e6b5123810c3903dd227c5bf6c53
Povilas Brilius <pbrilius@gmail.com>
2020-05-25 22:49:53 +0300

    Hashtag list  - API call - atomics; unit cases & controller.

---
+++ tests/CMS/Controller/API/Hashtag/HashtagListTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace Tests\API\Hashtag;
+
+use CMS\Controller\API\Hashtag\HashtagList;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Hashtag API stack
+ * 
+ * @category Unit_Cases
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class HashtagListTest extends TestCase
+{
+    /**
+     * Array return case
+     *
+     * @return void
+     */
+    public function testReturnType(): void
+    {
+        $hashtagList = $this
+            ->getMockBuilder(HashtagList::class)
+            ->disableOriginalConstructor()
+            ->disableProxyingToOriginalMethods()
+            ->setMethods(['__invoke'])
+            ->getMock();
+
+        $hashtagList
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn([]);
+
+        $request = $this
+            ->prophesize(ServerRequestInterface::class)
+            ->reveal();
+
+        $response = call_user_func(
+            $hashtagList,
+            $request,
+            []
+        );
+
+        $this->assertIsArray($response);
+        $this->assertEquals(0, sizeof($response));
+    }
+}
