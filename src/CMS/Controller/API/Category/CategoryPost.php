ffdecbb7be2e927fa57194479283cbb64a8afdf8
Povilas Brilius <pbrilius@gmail.com>
2020-05-26 12:06:11 +0300

    Category post atomics.

---
+++ src/CMS/Controller/API/Category/CategoryPost.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Controller
+ * @package  Invokables
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace CMS\Controller\API\Category;
+
+use CMS\BaseController;
+
+/**
+ * CMS stack
+ * 
+ * @category API
+ * @package  Category
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class CategoryPost extends BaseController
+{
+    /**
+     * Post invocation
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request Request
+     * @param array                                    $args    Arguments
+     * 
+     * @return array
+     */
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, array $args): array
+    {
+        $cmsModel = $this->getCmsModel();
+        $entity = $cmsModel->post($request->getParsedBody());
+
+        return $entity;
+    }
+}
