fc4f25b489df163da7d49049e331b224302991e0
Povilas Brilius <pbrilius@gmail.com>
2020-05-26 13:19:44 +0300

    Post post - add -  atomics.

---
+++ tests/CMS/Controller/API/Post/PostPostTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Controller
+ * @package  Invokables
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace Tests\CMS\Controller\API\Post;
+
+use CMS\Controller\API\Post\PostPost;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * CMS stack
+ * 
+ * @category API
+ * @package  Post
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class PostPostTest extends TestCase
+{
+    /**
+     * Callback case
+     *
+     * @return void
+     */
+    public function testCallback(): void
+    {
+        $postPost = $this
+            ->getMockBuilder(PostPost::class)
+            ->disableOriginalConstructor()
+            ->disableProxyingToOriginalMethods()
+            ->setMethods(['__invoke'])
+            ->getMock();
+
+        $postPost
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn([]);
+
+        $request = $this
+            ->prophesize(ServerRequestInterface::class)
+            ->reveal();
+
+        $response = call_user_func(
+            $postPost,
+            $request,
+            []
+        );
+
+        $this->assertIsArray($response);
+        $this->assertEquals(0, sizeof($response));
+    }
+}
