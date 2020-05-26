3e806d2b8044147597bdf9a7a73fa584a6695678
Povilas Brilius <pbrilius@gmail.com>
2020-05-26 21:00:35 +0300

    API ERP - resource update atomics.

---
+++ src/ERP/Controller/API/Resource/ResourceUpdate.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Controller
+ * @package  Invokables
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace ERP\Controller\API\Resource;
+
+use CMS\BaseController;
+
+/**
+ * ERP stack
+ * 
+ * @category API
+ * @package  Resource
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class ResourceUpdate extends BaseController
+{
+    /**
+     * Resource update
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request Request
+     * @param array                                    $args    Arguments
+     * 
+     * @return array
+     */
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, array $args): array
+    {
+        $cmsModel = $this->getCmsModel();
+
+        return $cmsModel->update($request->getQueryParams(), $args['id']);
+    }
+}
