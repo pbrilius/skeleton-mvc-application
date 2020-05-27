58b1bb99d78df08f44b90aeb17c662106e6e4488
Povilas Brilius <pbrilius@gmail.com>
2020-05-27 12:42:55 +0300

    Quality delete ERM API atomics.

---
+++ tests/ERM/Controller/API/QualityDeleteTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+
+namespace Tests\ERM\Controller\API;
+
+use App\Facilitator\BaseApiUnit;
+use ERM\Controller\Quality\QualityDelete;
+
+/**
+ * Quality API stack
+ * 
+ * @category Unit_Cases
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class QualityDeleteTeste extends BaseApiUnit
+{
+    /**
+     * Callback case
+     *
+     * @return void
+     */
+    public function testCallback(): void
+    {
+        $qualityDelete = $this
+            ->getMockBuilder(QualityDelete::class)
+            ->disableOriginalConstructor()
+            ->disableProxyingToOriginalMethods()
+            ->setMethods(['__invoke'])
+            ->getMock();
+
+        $qualityDelete
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn([]);
+
+        $response = call_user_func(
+            $qualityDelete,
+            $this->requestMock,
+            [
+                'id' => $this->id,
+            ]
+        );
+
+        $this->assertIsArray($response);
+        $this->assertEquals(0, sizeof($response));
+    }
+}
