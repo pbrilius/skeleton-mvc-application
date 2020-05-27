ee804241fde50ad73ac8a5e8de9d988c124c9567
Povilas Brilius <pbrilius@gmail.com>
2020-05-27 15:02:56 +0300

    Quantity list atomics case - ERM, API.

---
+++ tests/ERM/Controller/API/Quantity/QuantityListTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace Tests\ERM\Controller\API\Quantity;
+
+use App\Facilitator\BaseApiUnit;
+use ERM\Controller\Quantity\QuantityList;
+
+/**
+ * Quantity API stack
+ * 
+ * @category Unit_Cases
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class QuantityListTest extends BaseApiUnit
+{
+    /**
+     * Callback case
+     *
+     * @return void
+     */
+    public function testApiCallback(): void
+    {
+        $quantityList = $this
+            ->getMockBuilder(QuantityList::class)
+            ->disableOriginalConstructor()
+            ->disableProxyingToOriginalMethods()
+            ->setMethods(['__invoke'])
+            ->getMock();
+
+        $quantityList
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn([]);
+
+        $response = call_user_func(
+            $quantityList,
+            $this->requestMock,
+            []
+        );
+
+        $this->assertIsArray($response);
+        $this->assertEquals(0, sizeof($response));
+    }
+}
