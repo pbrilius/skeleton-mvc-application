dee592f0b5ac0d5615122bbb49dfd2aec353d720
Povilas Brilius <pbrilius@gmail.com>
2020-05-27 22:06:05 +0300

    CRM API customer stashed - loaded up - to a container.

---
+++ bootstrap/controllers/crm-api-customer-post.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Load
+ * @package  Launch
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+
+use CRM\Controller\API\Customer\CustomerPost;
+use CRM\Model\Customer as ModelCustomer;
+use League\Container\Container;
+
+/**
+ * Container
+ * 
+ * @var Container $container
+ */
+$container->add(
+    CustomerPost::class,
+    function () use ($container) {
+        $customerPost = new CustomerPost($container->get(ModelCustomer::class));
+
+        return $customerPost;
+    }
+);
