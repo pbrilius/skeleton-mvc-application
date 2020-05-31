691807d4d0363a4ef9d0b74e27791a3a696629b5
Povilas Brilius <pbrilius@gmail.com>
2020-05-31 17:07:23 +0300

    Conjob arranged - adjusted volume increase up to 24k records - entries.

--- src/App/Facilitator/BaseEtlTddUnit.php
+++ src/App/Facilitator/BaseEtlTddUnit.php
@@ -34,7 +34,7 @@ class BaseEtlTddUnit extends TestCase
      */
     private $_container;
 
-    protected $rate = 1024;
+    protected $rate = 1024 * 24;
 
     protected $uuidFormat = '/\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}/';
 
