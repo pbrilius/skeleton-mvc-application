1a7111d1eb229e6089e6967b8f981ffd08e05467
Povilas Brilius <pbrilius@gmail.com>
2020-06-01 22:09:06 +0300

    Account post API C2C atomics.

---
+++ tests/C2C/Controller/API/Account/AccountPostTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace Tests\C2C\Controller\API\Account;
+
+use App\Facilitator\BaseApiUnit;
+use C2C\Controller\API\Account\AccountPost;
+
+/**
+ * Account API stack
+ * 
+ * @category Unit_Cases
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class AccountPostTest extends BaseApiUnit
+{
+    /**
+     * Invocation case
+     *
+     * @return void
+     */
+    public function testInvocation(): void
+    {
+        $accountPost = $this
+            ->getMockBuilder(AccountPost::class)
+            ->disableOriginalConstructor()
+            ->disableProxyingToOriginalMethods()
+            ->disableArgumentCloning()
+            ->setMethods(['__invoke'])
+            ->getMock();
+
+        $accountPost
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn([]);
+
+        $response = call_user_func(
+            $accountPost,
+            $this->requestMock,
+            []
+        );
+
+        $this->assertIsArray($response);
+        $this->assertGreaterThanOrEqual(0, $response);
+    }
+}
