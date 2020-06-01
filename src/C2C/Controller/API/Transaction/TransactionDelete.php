c709790156abcfcae922d5708d71e4be15175a95
Povilas Brilius <pbrilius@gmail.com>
2020-06-01 23:00:19 +0300

    Transaction delete C2C API atomics.

---
+++ src/C2C/Controller/API/Transaction/TransactionDelete.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Controller
+ * @package  Invokables
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace C2C\Controller\API\Transaction;
+
+use CMS\BaseController;
+
+/**
+ * C2C stack
+ * 
+ * @category API
+ * @package  Transaction
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class TransactionDelete extends BaseController
+{
+    /**
+     * Invocation - C2C
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request Request
+     * @param array                                    $args    Arguments
+     * 
+     * @return array
+     */
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, array $args): array
+    {
+        $cmsModel = $this->getCmsModel();
+
+        return $cmsModel->delete($args['id']);
+    }
+}
---
+++ tests/C2C/Controller/API/Transaction/TransactionDeleteTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category TDD
+ * @package  BDD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+
+namespace Tests\C2C\Controller\API\Transaction;
+
+use App\Facilitator\BaseApiUnit;
+use C2C\Controller\API\Transaction\TransactionDelete;
+
+/**
+ * Transaction API stack
+ * 
+ * @category Unit_Cases
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class TransactionDeleteTest extends BaseApiUnit
+{
+    /**
+     * Invocation case
+     *
+     * @return void
+     */
+    public function testInvocation(): void
+    {
+        $transactionDelete = $this
+            ->getMockBuilder(TransactionDelete::class)
+            ->disableOriginalConstructor()
+            ->disableAutoReturnValueGeneration()
+            ->disableProxyingToOriginalMethods()
+            ->disableArgumentCloning()
+            ->setMethods(['__invoke'])
+            ->getMock();
+
+        $transactionDelete
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn([]);
+
+        $response = call_user_func(
+            $transactionDelete,
+            $this->requestMock,
+            [
+                'id' => $this->requestMock
+            ]
+        );
+
+        $this->assertIsArray($response);
+        $this->assertGreaterThanOrEqual(0, $response);
+    }
+}
