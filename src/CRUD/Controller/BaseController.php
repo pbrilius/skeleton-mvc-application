179707190481108cbc81dd27d915ef6fa88beb15
Povilas Brilius <pbrilius@gmail.com>
2020-06-05 17:19:06 +0300

    Base controller - CRUD case, atomics. PHP unit suites, Controller & View.

---
+++ src/CRUD/Controller/BaseController.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * PHP version 7
+ * 
+ * @category Base
+ * @package  Controller
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+namespace CRUD\Controller;
+
+use CMS\BaseControllerTrait as CmsTrait;
+use CMS\BaseModelInterface;
+use CRUD\View\EngineInterface;
+use PBG\Controller\BaseControllerInterface;
+
+/**
+ * Base controller for Embedded SBPC
+ * 
+ * @category SBPC
+ * @package  CRUD
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+abstract class BaseController implements BaseControllerInterface
+{
+    use CmsTrait;
+    use BaseControllerTrait;
+
+    /**
+     * Base default constructor
+     *
+     * @param BaseModelInterface $psrModel CMS model - PSR
+     * @param EngineInterface    $engine   Template engine - phtml
+     */
+    public function __construct(BaseModelInterface $psrModel, EngineInterface $engine)
+    {
+        $this->_cmsModel = $psrModel;
+        $this->_templateEngine = $engine;
+    }
+}
